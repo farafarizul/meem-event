5cc83c10845a27fdd7dd9f369d3536d7f5b9efae
Fara Farizul <farxpeace@gmail.com>
2026-03-12 04:16:54 +0800

    sdfsdfdsf

--- app/Services/CustomerProfileService.php
+++ app/Services/CustomerProfileService.php
@@ -27,25 +27,22 @@ class CustomerProfileService
         }
 
         $user = User::query()->where('meem_id', $data['id'])->first();
+
         if(!$user) {
             //create new user
-            User::create([
-                'meem_id'         => $data['id'] ?? null,
-
-        User::updateOrCreate(
-            ['meem_id' => $data['id'] ?? null],
-            [
-                'fullname'        => $data['name'] ?? null,
-                'email'           => $data['email'] ?? null,
-                'phone_number'    => $data['contact_no'] ?? null,
-                'meem_code'       => $data['cs_code'] ?? null,
-                'profile_picture' => $data['profile_picture'] ?? null,
+            $a = User::insert([
+                'fullname'     => fake()->name(),
+                'phone_number' => fake()->numerify('601########'),
+                'meem_code'    => 'MEEM' . str_pad(4, 6, '0', STR_PAD_LEFT),
+                'email'        => fake()->unique()->safeEmail(),
                 'password'     => Hash::make('password'),
                 'is_admin'     => false,
                 'created_at'   => now(),
                 'updated_at'   => now(),
-            ]);
-            return;
+                ]);
+
+            var_dump($a); exit();
+             return;
         }else{
             //update existing user
             $user->update([
