26c6746951f0173a2af8a35048683291619deb13
Fara Farizul <farxpeace@gmail.com>
2026-03-13 06:08:51 +0800

    sdfsdfdsf

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
         <div class="mb-3">
             <label for="email" class="form-label">Email Address</label>
             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                   name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
+                   name="email" value="{{ old('email') ?? "admin@meem.com.my" }}" required autofocus autocomplete="username">
             @error('email')
                 <div class="invalid-feedback">{{ $message }}</div>
             @enderror
@@ -28,6 +28,7 @@
         <div class="mb-3">
             <label for="password" class="form-label">Password</label>
             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                   value="12345678"
                    name="password" required autocomplete="current-password">
             @error('password')
                 <div class="invalid-feedback">{{ $message }}</div>
