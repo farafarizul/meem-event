441e2dda47a843edc3ff7d659854c8f8c63284d7
Fara Farizul <farxpeace@gmail.com>
2026-03-15 09:15:19 +0800

    sdfsdfsdf

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -49,7 +49,7 @@ class AuthController extends Controller
                 //generate random 20 character string, uppercase only for app_session
                 $app_session = strtoupper(substr(bin2hex(random_bytes(10)), 0, 20));
                 $device_info = $request->input('device_info', 'unknown');
-                print_r($device_info); exit();
+                //print_r($device_info); exit();
                 $body['data']['app_session'] = $app_session;
                 $body['data']['device_info'] = $request->input('device_info', 'unknown');
                 $log_data = $body['data'];
